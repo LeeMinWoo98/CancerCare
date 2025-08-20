d420260e66f8000b2d54f8a3348c6f1af2cb0df3
leeminwoo <lion021912@gmail.com>
2025-08-20 16:00:43 +0900

    프로젝트 구조

---
+++ src/main/java/org/example/Team6Application.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team6Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Team6Application.class, args);
+    }
+}
