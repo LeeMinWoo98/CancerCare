75d91490208acf1867e83a4e03f27b9c11132a89
leeminwoo <lion021912@gmail.com>
2025-08-21 14:07:49 +0900

    메인/회원가입/로그인 화면 수정_1

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -1,14 +1,28 @@
 package org.example.controller;
 
 
+import org.example.form.LoginForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
     @GetMapping("/")
     public String home() {
         return "main";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String sign(){
+        return "signup";
     }
 
 }
